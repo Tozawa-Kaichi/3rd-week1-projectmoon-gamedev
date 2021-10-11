e3cbe981b728b2f71e6f2bc60f87b0c811ee50d5
戸澤海智 <st37071556@icloud.com>
2021-10-11 13:36:05 +0900

    授業開始

---
+++ Assets/Script/DarumaShoot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarumaShoot : MonoBehaviour
+{
+    Rigidbody m_darumaRB = default;
+    bool m_Ground = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_darumaRB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && m_Ground)
+        {
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        m_Ground = true;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        m_Ground = false;
+    }
+
+}
