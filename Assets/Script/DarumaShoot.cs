7f783035f927bca14940df67aa1e482ccf775d11
戸澤海智 <st37071556@icloud.com>
2021-10-11 14:32:50 +0900

    ３

--- Assets/Script/DarumaShoot.cs
+++ Assets/Script/DarumaShoot.cs
@@ -6,10 +6,13 @@ public class DarumaShoot : MonoBehaviour
 {
     Rigidbody m_darumaRB = default;
     bool m_Ground = false;
+    Vector3 m_Moveforce = default;
+    float x = 5;
     // Start is called before the first frame update
     void Start()
     {
         m_darumaRB = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
@@ -17,17 +20,21 @@ public class DarumaShoot : MonoBehaviour
     {
         if (Input.GetButtonDown("Jump") && m_Ground)
         {
-
+            m_darumaRB.AddForce(transform.forward *200.0f, ForceMode.Impulse);
+            m_Ground = false;
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        m_Ground = true;
+        if (collision.gameObject.tag == "Ground")
+        {
+            m_Ground = true;
+        }
     }
     private void OnCollisionExit(Collision collision)
     {
-        m_Ground = false;
+       //m_Ground = false;
     }
 
 }
